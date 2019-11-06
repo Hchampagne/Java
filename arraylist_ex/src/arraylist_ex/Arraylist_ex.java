88b61d1f21f7316e3e6a0779b3799acd5b0d17f2
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2019-11-06 10:53:38 +0100

    fin arraylist et modif nbvoyelle

--- arraylist_ex/src/arraylist_ex/Arraylist_ex.java
+++ arraylist_ex/src/arraylist_ex/Arraylist_ex.java
@@ -6,7 +6,7 @@
 package arraylist_ex;
 
 /**
- *
+ * V1.00 du 05/11/2019
  * @author 80010-92-01
  */
 
@@ -18,39 +18,116 @@ public class Arraylist_ex {
    
     public static void main(String[] args) {
         
-        String choix;
         String donnee;
         String prenom;
-        boolean flag;
-        
-        Scanner lectureClavier = new Scanner(System.in);
+        String n ;
+        String ajout;
+        String nouv;
+        String suppr;
+        String modif;
         
         //creation tableau
         ArrayList<String> tab = new ArrayList<String>();
-        // entrez des données
-        System.out.println("Entrez les données (exit pour sortir)");        
-        do{
-        System.out.print("Donnée : ");
-        donnee = lectureClavier.nextLine();
-        flag = !donnee.equals("exit");  //def  flag pour condition if et arret while
-        if (flag){tab.add(donnee);}  // entre dans tableau si diff exit 
-        }while(flag);  // sort sur exit saisi
-        // list tableau
-        for(String i : tab){
-            System.out.print(i+" ," );
-        }
         
-        //modif du tableau
-        System.out.println("Entrez le prenom"); 
-        prenom = lectureClavier.nextLine();
+        boolean flag;
         
-        for (int i=0 ; i<tab.size()-1 ; i++){
-            if(tab.get(i).equals(prenom)){}
-        }
+        // instanciation scanner pour entrée clavier
+        Scanner lectureClavier = new Scanner(System.in);
         
+        System.out.println("");
+        System.out.println("---------------------------------");
+        System.out.println("| 1 - Création du tableau       |");
+        System.out.println("| 2 - ajout                     |");
+        System.out.println("| 3 - modifier                  |");
+        System.out.println("| 4 - supprimer                 |");
+        System.out.println("| 5 - afficher                  |");
+        System.out.println("| pour sortir exit              |");
+        System.out.println("---------------------------------");
+        System.out.println(""); 
+        
+        
+        do{ //boucle pour menu
+       
+        System.out.print("entrez votre choix : "); // entrée choix       
+        n =lectureClavier.nextLine();
+        System.out.println("");
+        System.out.println("*********************************");
+        System.out.println("");
+        
+            switch(n){   //choix du menu
+                case "1" :                      
+                    // entrez des données ds tableau
+                    System.out.println("Entrez les données");        
+                    do{
+                    System.out.print("Donnée : ");
+                    donnee = lectureClavier.nextLine();
+                    flag = !donnee.equals("exit");  //def  flag pour condition if et arret while
+                    if (flag){tab.add(donnee);}  // entre dans tableau si diff exit 
+                    }while(flag);  // sort sur exit saisi
+                    break;
+                
+                case "2" :   
+                    // ajout fin tableau
+                    do{
+                    System.out.print("Entrez le prenom à ajouter : "); //entre le prenom
+                    prenom = lectureClavier.nextLine();              
+                    tab.add(prenom);    //ajout au tableau
+                    System.out.print("encore un ajout (o/n) ");
+                    ajout = lectureClavier.nextLine();
+                    }while(!ajout.equals("n"));
+                    break;
+                
+                case "3":
+                    //modifer une valeur
+                    if(tab.size() != 0){
+                        System.out.print("Entrez le prenom à modifier : "); //entre le prenom
+                        modif= lectureClavier.nextLine(); 
+                   
+                        for (int i=0 ; i<=tab.size()-1 ; i++){  //modif valeur tableau
+                            if(tab.get(i).equals(modif)){
+                                System.out.print("Entrez le nouveau  prenom : ");
+                                nouv = lectureClavier.nextLine(); 
+                                tab.set(i,nouv);
+                            }
+                        } 
+                    }else{
+                        System.out.println("Tableau vide"); 
+                    }
+                    break;
+                
+                        
+                case "4":    
+                    //suppr du tableau
+                    if(tab.size() != 0){
+                        System.out.print("Entrez le prenom à supprimer : "); //entre le prenom
+                        suppr = lectureClavier.nextLine();              
+                        for (int i=0 ; i<=tab.size()-1 ; i++){  //supprimer le prenom
+                            if(tab.get(i).equals(suppr)){
+                                tab.remove(i);
+                            }
+                        }
+                    }else{
+                        System.out.println("Tableau vide");    
+                    }
+                    break;
+                               
+                case "5": 
+                    // list tableau
+                    for(String x : tab){
+                        System.out.println( x + "," );                      
+                    }                   
+                    break;    
+            
+                default :
+                    // si choix incorrecte
+                    if(!n.equals("exit")){
+                    System.out.println("choix incorrecte !");
+                    }
+                    break;
+            }
               
-        
-        
+        }while(!n.equals("exit"));  
+        System.out.println("GOOD BYE !");
     }
     
 }
--- nbVoyelle/src/nbvoyelle/NbVoyelle.java
+++ nbVoyelle/src/nbvoyelle/NbVoyelle.java
@@ -16,27 +16,30 @@ public class NbVoyelle {
     public static void main(String[] args) {
       
         String mot;
-        int nbLettre;
-        int nbVoyelle =0;
-        String car;
-        int i;
+        String choix;
+        int nbLettre;       
         String test = "aeuioéèàyêëù";
                                   
-        Scanner lectureClavier = new Scanner(System.in);        
-        System.out.println("Entrez N : ");
+        Scanner lectureClavier = new Scanner(System.in);  
+        
+        do{
+        int nbVoyelle = 0;
+            
+        System.out.print("Entrez le mot : ");
         mot = lectureClavier.nextLine(); 
          
-        nbLettre = mot.length();
+        nbLettre = mot.length();       
+        System.out.println("Nombre de lettre(s) : "+nbLettre);       
         
-        System.out.print("Nombre de lettre(s) : ");
-        System.out.println(nbLettre);
-        
-        for(i=0;i<nbLettre;i++){
-        car = mot.substring(i,i+1);
-        if(test.contains(car)){nbVoyelle = nbVoyelle + 1;}       
-        }
-        System.out.println("");
+        for(int i=0;i<nbLettre;i++){
+        if(test.contains(mot.substring(i,i+1))){nbVoyelle++;}       
+        }        
         System.out.println("Nombre de voyelle(s) : "+nbVoyelle);
+        
+        System.out.print("Encore un essai (o/n) : ");
+        choix = lectureClavier.nextLine();
+        System.out.println(" ");
+        }while(choix.equals("o") );
     }
     
 }
