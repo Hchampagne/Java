17be61a7837bcdfb70d9b919ce3a791948961db3
hchampagne <hchampagne56@gmail.com>
2019-11-13 21:56:41 +0100

    javafx

---
+++ javafx/test/src/test/Test.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author Doudou
+ */
+public class Test extends Application {
+	
+ 
+	
+    public Test() 
+    {	
+        System.out.println("constructeur ...");	
+    }
+	
+ 
+	
+    @Override
+	
+    public void start(Stage primaryStage) throws Exception 
+    {	
+        System.out.println("start ... ");
+	Label label = new Label("No country ...");	
+        label.relocate(70, 20);	
+        label.setStyle("-fx-font-size: 30px;");	
+        Pane root = new Pane();	
+        root.getChildren().add(label);	
+        Scene scene = new Scene( root , 300 , 100 , Color.GREY);	
+        primaryStage.setTitle( "Un titre") ;	
+        primaryStage.setScene(scene) ;	
+        primaryStage.show() ;
+    }
+		
+    public static void main(String[] args) 
+    {	
+        System.out.println("main ...");
+        Application.launch(args);	
+    }
+	
+ 
+	
+    @Override
+	
+    public void init() throws Exception 
+    {
+        System.out.println("init ...");	
+    }	
+}
+
+    
+    
+
---
+++ javafx/testfx/src/testfx/Testfx.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfx;
+
+/**
+ * v1.00 13/1/2019
+ * @author Doudou
+ */
+public class Testfx {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
