20b34e616d1b7f9d05cd3ac590cf02e10173893b
hchampagne <hchampagne56@gmail.com>
2019-11-18 22:22:44 +0100

    m

--- beans11/caseCocher/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.casecocher {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.casecocher to javafx.fxml;
-    exports com.mycompany.casecocher;
-}
