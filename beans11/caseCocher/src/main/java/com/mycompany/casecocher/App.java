81b01e8ba90d3dfca42338583ec95c2ba3d02c56
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2019-11-19 16:45:23 +0100

    ex2

--- beans11/caseCocher/src/main/java/com/mycompany/casecocher/App.java
+++ beans11/caseCocher/src/main/java/com/mycompany/casecocher/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("form"), 1200, 900);
+        scene = new Scene(loadFXML("form"), 600, 600);
         stage.setScene(scene);
         stage.show();
     }
--- beans11/caseCocher/src/main/java/com/mycompany/casecocher/FormController.java
+++ beans11/caseCocher/src/main/java/com/mycompany/casecocher/FormController.java
@@ -7,7 +7,14 @@ package com.mycompany.casecocher;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 
 /**
  * FXML Controller class
@@ -16,14 +23,152 @@ import javafx.fxml.Initializable;
  */
 public class FormController implements Initializable {
 
+    @FXML
+    private TextField saisie;
+    @FXML
+    private Label label;
+    @FXML
+    private TitledPane choix;
+    @FXML
+    private CheckBox c_fond;
+    @FXML
+    private CheckBox c_texte;
+    @FXML
+    private CheckBox c_casse;
+    @FXML
+    private TitledPane texte;
+    @FXML
+    private TitledPane casse;
+    @FXML
+    private RadioButton t_rouge;
+    @FXML
+    private RadioButton f_rouge;
+    @FXML
+    private RadioButton f_vert;
+    @FXML
+    private RadioButton f_bleu;
+    @FXML
+    private TitledPane fond;
+    @FXML
+    private RadioButton t_blanc;
+    @FXML
+    private RadioButton t_noir;
+    @FXML
+    private RadioButton min;
+    @FXML
+    private RadioButton maj;
+    
+    
     /**
      * Initializes the controller class.
      * @param url
      * @param rb
+     * desactive choix/fond/texte/casse
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        choix.setDisable(true);
+        fond.setDisable(true);
+        texte.setDisable(true);
+        casse.setDisable(true);
     }    
+// saisie et transf ds label active choix
+    @FXML
+    private void saisie_change(ActionEvent event) {
+        label.setText(saisie.getText());
+        choix.setDisable(false);
+    }
+    //active fond si select fond
+    @FXML
+    private void c_fond_select(ActionEvent event) {
+        fond.setDisable(false);
+    }
+    //active texte si select casse
+    @FXML
+    private void c_casse_select(ActionEvent event) {
+        casse.setDisable(false);
+    }
+    //active texte si select texte
+    @FXML
+    private void c_texte_select(ActionEvent event) {
+        texte.setDisable(false);
+    }
+//gestion fond
+    //fond rouge
+    @FXML
+    private void f_rouge_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-background-color: red;");
+        f_rouge.setSelected(true);
+        f_vert.setSelected(false);
+        f_bleu.setSelected(false);
+    }
+    //fond vert
+    @FXML
+    private void f_vert_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-background-color: green;");
+        f_rouge.setSelected(false);
+        f_vert.setSelected(true);
+        f_bleu.setSelected(false);
+        
+    }
+    //fond bleu
+    @FXML
+    private void f_bleu_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-background-color: blue;");
+        f_rouge.setSelected(false);
+        f_vert.setSelected(false);
+        f_bleu.setSelected(true);
+    }
+//gestion texte
+    //texte rouge
+    @FXML
+    private void t_rouge_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-text-fill: red;");
+        t_rouge.setSelected(true);
+        t_blanc.setSelected(false);
+        t_noir.setSelected(false);
+    }
+    //texte blanc
+    @FXML
+    private void t_blanc_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-text-fill: white;");
+        t_rouge.setSelected(false);
+        t_blanc.setSelected(true);
+        t_noir.setSelected(false);
+    }
+    //texte noir
+    @FXML
+    private void t_noir_select(ActionEvent event) {
+        String style = label.getStyle();
+        label.setStyle(style+"-fx-text-fill: black;");
+        t_rouge.setSelected(false);
+        t_blanc.setSelected(false);
+        t_noir.setSelected(true);
+    }
+// gestion maj/minus
+    //minus
+    @FXML
+    private void min_select(ActionEvent event) {
+        
+        String text = label.getText();
+        label.setText(text.toLowerCase());
+        min.setSelected(true);
+        maj.setSelected(false);
+    }
+    //maj
+    @FXML
+    private void maj_select(ActionEvent event) {
+        
+        String text = label.getText();
+        label.setText(text.toUpperCase());
+        min.setSelected(false);
+        maj.setSelected(true);
+    }
+    
     
 }
