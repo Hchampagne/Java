108c4032734c234bab1345929b7042c808d5dca7
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2019-12-05 16:54:31 +0100

    fin java/CRUD reste commentaires et factoristion

--- beans11/crud_proj/src/main/java/com/mycompany/crud_proj/ClientDAO.java
+++ beans11/crud_proj/src/main/java/com/mycompany/crud_proj/ClientDAO.java
@@ -13,148 +13,175 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
  * @author 80010-92-01
  */
 public class ClientDAO {
-    
-    public ClientDAO(){
+
+    public ClientDAO() {
     }
- 
+
     //LISTE
-    public List<Client> List(){               
-        List<Client> resultat = new ArrayList<>();      
-        try {
-            String url_db ="jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-            try (Connection con = DriverManager.getConnection(url_db, "root", "")) {
-                ResultSet result;
-                try (Statement stm = con.createStatement()) {
-                    result = stm.executeQuery("SELECT * FROM client");
-                    while (result.next()) {
-                        Client c = new Client();
-                        c.setId(result.getInt("cli_id"));
-                        c.setNom(result.getString("cli_nom"));
-                        c.setPrenom(result.getString("cli_prenom"));
-                        c.setVille(result.getString("cli_ville"));
-                        resultat.add(c);
-                    }
-                }
+    public List<Client> List() {
+        List<Client> resultat = new ArrayList<>();       
+        String url_db = "jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+        Connection con ;
+        ResultSet result;
+        Statement stm;
+            try{ 
+                con = DriverManager.getConnection(url_db, "root", "");              
+                stm = con.createStatement();
+                result = stm.executeQuery("SELECT * FROM client");
+                
+                while (result.next()) {
+                    Client c = new Client();
+                    c.setId(result.getInt("cli_id"));
+                    c.setNom(result.getString("cli_nom"));
+                    c.setPrenom(result.getString("cli_prenom"));
+                    c.setVille(result.getString("cli_ville"));
+                    resultat.add(c);
+                }               
                 result.close();
-                con.close();
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while reading 'client'");
-            System.out.println(e.getMessage());
-        }        
+                con.close();          
+            } catch (SQLException e) {
+                
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setHeaderText("AJOUT");
+                alert.setContentText("Erreur acces pour la liste /n."+ e.getMessage());
+                alert.show();
+        }
         return resultat;
     }
-    
-    
+
     //Ajout
-    public void Insert(Client cli){
-        String url_db ="jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+    public void Insert(Client cli) {
+        String url_db = "jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
         Connection con;
-        try {
-            con = DriverManager.getConnection(url_db, "root", "");
-            try (PreparedStatement stm = con.prepareStatement("INSERT INTO client (cli_nom, cli_prenom, cli_ville) VALUES (?,?,?)")) {
+        PreparedStatement stm;
+        
+            try {
+                con = DriverManager.getConnection(url_db, "root", "");
+                stm = con.prepareStatement("INSERT INTO client (cli_nom, cli_prenom, cli_ville) VALUES (?,?,?)");
                 stm.setString(1, cli.getNom());
-                stm.setString(2,cli.getPrenom());
-                stm.setString(3,cli.getVille());
-                
+                stm.setString(2, cli.getPrenom());
+                stm.setString(3, cli.getVille());
+
                 stm.execute();
+
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setHeaderText("AJOUT");
+                alert.setContentText("Ajout effectué.");
+                alert.show();
+
                 con.close();
                 stm.close();
-            }
             
-        } catch (SQLException e) {
-            System.out.println("Error while inserting entity 'client'");
-            System.out.println(e.getMessage());
+
+            } catch (SQLException e) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setHeaderText("AJOUT");
+                alert.setContentText("Erreur pendant l'ajout /n."+ e.getMessage());
+                alert.show();
         }
-    }    
-        
+    }
+
     //Suppression
-    public void Delete(Client cli){
-         String url_db ="jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-        try {
-            Connection con = DriverManager.getConnection(url_db, "root", "");
-            PreparedStatement del = con.prepareStatement("DELETE FROM client WHERE cli_id = ?");
+    public void Delete(Client cli) {
+        String url_db = "jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+        Connection con ;
+        PreparedStatement del ;
         
-            del.setInt(1,cli.getId());
+        try {
+            con = DriverManager.getConnection(url_db, "root", "");
+            del = con.prepareStatement("DELETE FROM client WHERE cli_id = ?");
+
+            del.setInt(1, cli.getId());
+            
             del.execute();
             con.close();
             del.close();
-            
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setHeaderText("SUPPRIMER");
+            alert.setContentText(" Suppression effectuée.");
+            alert.show();
+
         } catch (SQLException e) {
-            System.out.println("Error while deleting entity 'client'");
-            System.out.println(e.getMessage());
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("AJOUT");
+            alert.setContentText("Erreur pendant la suppression /n."+ e.getMessage());
+            alert.show();
         }
     }
-    
+
     //Modification
-    public void Update(Client cli){
-        String url_db ="jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+    public void Update(Client cli) {
+        String url_db = "jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
         Connection con;
+        PreparedStatement mod ;
+        
         try {
             con = DriverManager.getConnection(url_db, "root", "");
-            PreparedStatement mod = con.prepareStatement("UPDATE client SET cli_nom = ?, cli_prenom = ?, cli_ville = ?  WHERE cli_id = ?");
-                      
+            mod = con.prepareStatement("UPDATE client SET cli_nom = ?, cli_prenom = ?, cli_ville = ?  WHERE cli_id = ?");
+
             mod.setString(1, cli.getNom());
             mod.setString(2, cli.getPrenom());
             mod.setString(3, cli.getVille());
             mod.setInt(4, cli.getId());
-            
+
             mod.execute();
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setHeaderText("MODIFICATION");
+            alert.setContentText("Modification effectuée.");
+            alert.show();
+
             mod.close();
             con.close();
-            
+
         } catch (SQLException e) {
-            System.out.println("Error while updating entity 'client'");
-            System.out.println(e.getMessage());
+            
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("MODIFICATION");
+            alert.setContentText("Erreur pendant la modification /n." + e.getMessage() );
+            alert.show();   
         }
-     
     }
-    
-    //Détail
-    public Client Find(int id){
         
+
+    //Détail
+    public Client Find(int id) {
+
         ///System.out.println("id pour détail : " + id);
-         
-        String url_db ="jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+        String url_db = "jdbc:mysql://localhost:3306/hotel?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
         Connection con;
         ResultSet select;
-        Client row = new Client();    
+        Client row = new Client();
         try {
             con = DriverManager.getConnection(url_db, "root", "");
-            PreparedStatement sel = con.prepareStatement("SELECT cli_id, cli_nom, cli_prenom, cli_ville FROM client WHERE cli_id = ?"); 
-                sel.setInt(1, id);
-                select = sel.executeQuery(); 
-                
-                while(select.next()){                    
-                    row.setId(select.getInt("cli_id"));
-                    row.setNom(select.getString("cli_nom"));
-                    row.setPrenom(select.getString("cli_prenom"));
-                    row.setVille(select.getString("cli_ville"));
-                                       
-                    //System.out.println("nom : " + (select.getString("cli_nom")));
-                    //System.out.println("nom : " + (select.getString("cli_prenom")));
-                    //System.out.println("nom : " + (select.getString("cli_ville")));                               
-                }
-            
-            con.close(); 
-            
-        } catch (SQLException e) {
-            System.out.println("Error while finding entity 'client'");
-            System.out.println(e.getMessage());
-        }
-      return row; 
-    }
-}    
-    
-    
-    
-    
-           
-    
+            PreparedStatement sel = con.prepareStatement("SELECT cli_id, cli_nom, cli_prenom, cli_ville FROM client WHERE cli_id = ?");
+            sel.setInt(1, id);
+            select = sel.executeQuery();
 
+            while (select.next()) {
+                row.setId(select.getInt("cli_id"));
+                row.setNom(select.getString("cli_nom"));
+                row.setPrenom(select.getString("cli_prenom"));
+                row.setVille(select.getString("cli_ville"));                            
+            }
+
+            con.close();
+
+        } catch (SQLException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("DeTAIL");
+            alert.setContentText("Erreur pendant la requete détail /n." + e.getMessage() );
+            alert.show();
+        }
+        return row;
+    }
+}
--- beans11/crud_proj/src/main/java/com/mycompany/crud_proj/ViewController.java
+++ beans11/crud_proj/src/main/java/com/mycompany/crud_proj/ViewController.java
@@ -13,7 +13,10 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -30,6 +33,7 @@ public class ViewController implements Initializable {
     
     int index ;
     int id ;
+    String role ;
     
 
     @FXML
@@ -43,8 +47,6 @@ public class ViewController implements Initializable {
     @FXML
     private TableView<Client> tableview;
     @FXML
-    private Button suppr;
-    @FXML
     private TextField t_nom;
     @FXML
     private TextField t_prenom;
@@ -53,9 +55,17 @@ public class ViewController implements Initializable {
     @FXML
     private Button modif;
     @FXML
+    private Button annuler;
+    @FXML
+    private Button ok;
+    @FXML
+    private Button ajouter;
+    @FXML
     private Pane cache;
     @FXML
-    private Button annuler;
+    private Button supprimer;
+    @FXML
+    private Label mess_error;
 
     /**
      * Initializes the controller class.
@@ -65,16 +75,19 @@ public class ViewController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        
+       
+        
+        
         //instancie classe ClientDAO
-        ClientDAO liste = new ClientDAO();
-        
-        // recup return dans model
-        List<Client> model = liste.List();
-        
+        ClientDAO liste = new ClientDAO();        
+        // recup le return dans model
+        List<Client> model = liste.List();       
         // cast la "arraylist" en observablelist pour tableview 
         ObservableList<Client> mod = FXCollections.observableArrayList(model);
         
-        //lie les colonne avec nom variable pour extraction table view
+        //lie les colonnes avec nom variable pour insertion dans tableview
         c_id.setCellValueFactory(new PropertyValueFactory<>("id"));
         c_prenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));	
         c_nom.setCellValueFactory(new PropertyValueFactory<>("nom"));	
@@ -84,7 +97,7 @@ public class ViewController implements Initializable {
         // empeche edition table
         tableview.setEditable(false);
         
-        //rempli le tableau tableview     
+        //rempli le tableview     
         tableview.setItems(mod);       
     }      
 
@@ -94,37 +107,181 @@ public class ViewController implements Initializable {
     Sur click souris selection dans tableau recup index tableau
     => id de l'enregistrement.
     */    
-        index = tableview.getSelectionModel().getSelectedIndex(); // recup index       
+        index = tableview.getSelectionModel().getSelectedIndex(); // recup index 
+           
         id = (int) tableview.getColumns().get(0).getCellObservableValue(index).getValue();  //recup id col=0 dans get
-        //System.out.println("index = " + index + "   id = " + id); 
+             
+        if (!"add".equals(role)){// si selection addition ne garni pas les textview
+        // intancie class dao pour recup row fct id   
+        ClientDAO find = new ClientDAO();
+        Client row = find.Find(id); 
+        // garni les textview
+        t_nom.setText(row.nom);   
+        t_prenom.setText(row.prenom);
+        t_ville.setText(row.ville);
+        }     
     }
 
     @FXML
     private void click_supr(ActionEvent event) {
         
-        cache.setVisible(false);
-        ClientDAO find = new ClientDAO();
-        Client row = find.Find(id);         
-        //System.out.println(row.id + "  " + row.nom + "  " + row.prenom + "  " + row.ville);         
-        t_nom.setText(row.nom);
-        t_prenom.setText(row.prenom);
-        t_ville.setText(row.ville);
+        // fourni le role = del appuie bouton suppression (switch/case)
+        role = "del";       
+        //  textview / non modifiable
+        textview();               
     }
 
     @FXML
     private void click_modif(ActionEvent event) {
-        cache.setVisible(false);
-        ClientDAO find = new ClientDAO();
-        Client row = find.Find(id);                  
-        t_nom.setText(row.nom);
-        t_prenom.setText(row.prenom);
-        t_ville.setText(row.ville);
+        
+        //fournis le role = mod appuie bouton modification (switc/case)
+        role = "mod";      
+        //  textview  / modifiable
+        textview();
+             
+    }
+    
+        @FXML
+    private void click_ajouter(ActionEvent event) {
+        
+        //fournis le role = mod appuie bouton ajout (switc/case)
+        role = "add";       
+        // clear textview / modifiable
+        textview();     
     }
 
     @FXML
     private void click_annuler(ActionEvent event) {
-        cache.setVisible(true);
+        
+        //fournis le role = "" appuie bouton annuler 
+        role = "";
+        //vide les textview et cache menu de droite
+        textview();      
+    }
+   
+
+    @FXML
+    private void click_ok(ActionEvent event) {
+        
+        switch(role){
+            
+            case "add" : 
+                // instancie un client
+                Client insert = new Client();
+                insert.nom = t_nom.getText();
+                insert.prenom = t_prenom.getText();
+                insert.ville = t_ville.getText();
+                
+                if(!"".equals(t_nom.getText())  && !"".equals(t_prenom.getText()) && !"".equals(t_ville.getText())){
+                // les champs ne sont pas vide
+                // instancie classe dao et insert base de donnée
+                ClientDAO ins = new ClientDAO();
+                ins.Insert(insert);
+                //actualise la liste
+                actual();
+                }else{
+                   //les champs sont vide => popup  message erreur
+                   Alert alert = new Alert(AlertType.ERROR);
+                   alert.setHeaderText("Ajout à la base");
+                   alert.setContentText("Attention champs vide(s) ! ");
+                   alert.show();
+                }
+                
+                break ;
+                
+            case "mod" : 
+                // instancie un client
+                Client up = new Client();
+                up.id = id;               
+                up.nom = t_nom.getText();
+                up.prenom = t_prenom.getText();
+                up.ville = t_ville.getText(); 
+                
+                if(!"".equals(t_nom.getText())  && !"".equals(t_prenom.getText()) && !"".equals(t_ville.getText())){
+                // les champs ne sont pas vide
+                // instancie classe dao et update base de donnée   
+                ClientDAO update = new ClientDAO(); 
+                update.Update(up);
+                //actualise la liste
+                actual();
+                }else{
+                   //les champs sont vide => popup  message erreur
+                   Alert alert = new Alert(AlertType.ERROR);
+                   alert.setHeaderText("Modification de la base");
+                   alert.setContentText("Attention champs vide(s) ! ");
+                   alert.show();
+                }
+                break;
+            
+            case "del" :
+                // instancie un client juste sur id
+                Client suppr = new Client();
+                suppr.id = id; 
+                // les champs ne sont pas vide
+                // instancie classe dao et delete row dans base de donnée
+                ClientDAO del = new ClientDAO();
+                del.Delete(suppr); ;                
+                //actualise la liste
+                actual();
+                break ;
+                
+            default :  break ;
+        
+        }           
     }
     
+    public void actual(){
+           
+        //instancie classe ClientDAO
+        ClientDAO liste = new ClientDAO();        
+        // recup le return dans model
+        List<Client> model = liste.List();       
+        // cast la "arraylist" en observablelist pour tableview 
+        ObservableList<Client> mod = FXCollections.observableArrayList(model);
+        //rempli le tableview     
+        tableview.setItems(mod); 
+    }
     
-}
+    public void textview(){
+        
+        if (!"".equals(role)){ //si appuie bouton annuler =>else
+        
+        //rend visible les textview
+        cache.setVisible(false);
+        
+        //intancie class dao pour recup row fct id  
+        ClientDAO find = new ClientDAO();
+        Client row = find.Find(id);
+        
+        // garni les textview et les rend non modifiable
+        t_nom.setText(row.nom);
+        t_prenom.setText(row.prenom);
+        t_ville.setText(row.ville);
+        
+        // disable les chmaps textview pour role=mod
+        if("del".equals(role)){t_nom.setDisable(true);}else{t_nom.setDisable(false);}
+        if("del".equals(role)){t_prenom.setDisable(true);}else{t_prenom.setDisable(false);}
+        if("del".equals(role)){t_ville.setDisable(true);}else{t_ville.setDisable(false);}
+        
+        //pour role add clear les textview
+        if("add".equals(role)){t_nom.clear();}
+        if("add".equals(role)){t_prenom.clear();}
+        if("add".equals(role)){t_ville.clear();}
+        
+        }else{
+            // active le cache menu droite
+            cache.setVisible(true);
+            
+            //clear textview
+            t_nom.clear();
+            t_prenom.clear();
+            t_ville.clear();       
+        }
+        
+    }
+    }
+
+   
+    
+    
+
